5b222e2e873f4333b8910650e3dc7da37ca7caf8
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2018-01-16 18:46:20 +0200

    removing the cat image, I didn't feel it is nice

--- js/index.js
+++ js/index.js
@@ -10,21 +10,21 @@ function calculate() {
 	result = (((ribCage/0.7067)-limValue)/0.9156)-limValue
 
     document.getElementById("result").innerHTML =  "CBMI is = " + result;
-    document.getElementById("catimage").style.visibility = "visible";
+    // document.getElementById("catimage").style.visibility = "visible";
 
     if (result>30) {
-	    document.getElementById("catimage").style.width = "100%";
+	    // document.getElementById("catimage").style.width = "100%";
 	    document.getElementById("catstatus").innerHTML = "Please visit the nearest vet. "+ catName + " needs Help.";
 	}
     else{
     	if (result<12) {
-    		document.getElementById("catimage").style.width = "30%";
+    		// document.getElementById("catimage").style.width = "30%";
     		document.getElementById("catstatus").innerHTML = "Please visit the nearest vet. "+ catName + " needs Help.";
     	}
     	else
     	{
-    		document.getElementById("catimage").style.width = "70%";
-    		document.getElementById("catstatus").innerHTML = catName + " is normal";
+    		// document.getElementById("catimage").style.width = "70%";
+    		document.getElementById("catstatus").innerHTML = catName + " is Normal";
 
     	}
     }
