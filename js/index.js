e18a8b4e5e28aab15cea268809dd23766bd865d8
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2018-01-16 16:50:09 +0200

    Adding the calculation of the cbmi in js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 function calculate() {
-    document.getElementById("result").innerHTML = "Hello World";
+	ribCage = document.getElementById("cocname").value;
+	limValue = document.getElementById("limname").value;
+	result = (((ribCage/0.7067)-limValue)/0.9156)-limValue
+
+    document.getElementById("result").innerHTML = result;
+    
 }
